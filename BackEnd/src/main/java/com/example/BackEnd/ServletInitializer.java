212c40b0e74150e6f8b2c240f6f43aa11104ab95
[mac] euihyune <lobil7782@gmail.com>
2022-12-04 18:38:56 +0900

    [ADD] 기존 파일 삭제 후 BackEnd 추가

--- BackEnd/src/main/java/com/example/BackEnd/ServletInitializer.java
+++ BackEnd/src/main/java/com/example/BackEnd/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example.Javis;
+package com.example.BackEnd;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(JavisApplication.class);
+		return application.sources(BackEndApplication.class);
 	}
 
 }
--- BackEnd/src/test/java/com/example/BackEnd/BackEndApplicationTests.java
+++ BackEnd/src/test/java/com/example/BackEnd/BackEndApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.Javis;
+package com.example.BackEnd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JavisApplicationTests {
+class BackEndApplicationTests {
 
 	@Test
 	void contextLoads() {
