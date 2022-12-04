c901495da20d0741f81d56a663e8a25c99889d54
[mac] euihyune <lobil7782@gmail.com>
2022-12-04 18:23:04 +0900

    [ADD] Repository

---
+++ Javis/src/test/java/com/example/Javis/JavisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Javis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
