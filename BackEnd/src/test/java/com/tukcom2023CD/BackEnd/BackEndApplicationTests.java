104b6003a75a88bff96ce9670f2cc577339dfe9f
euihyuneee <lobil7782@gmail.com>
2023-03-02 15:39:41 +0900

    [ADD] BackEnd repo

---
+++ BackEnd/src/test/java/com/tukcom2023CD/BackEnd/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tukcom2023CD.BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
