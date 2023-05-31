c97cdb9091c9cbe2b72fd5015f83f7a63bdd579e
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-06-01 03:54:19 +0900

    [UPDATE] rest api_serializers 추가

--- BackEnd/Django/modelImport/product/serializers.py
+++ BackEnd/Django/modelImport/product/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Style, Emoji
+class StyleSerializer(serializers.ModelSerializer):
+    img = serializers.ImageField(use_url=True)
 
-class ProductSerializer(serializers.ModelSerializer) :
-    class Meta :
-        model = Product
+    class Meta:
+        model = Style
+        fields = '__all__'
+
+
+class EmojiSerializer(serializers.ModelSerializer):
+    emoji = serializers.ImageField(use_url=True)
+
+    class Meta:
+        model = Emoji
         fields = '__all__'
