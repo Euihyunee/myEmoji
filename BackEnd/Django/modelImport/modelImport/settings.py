72a5b0c9a8263cc847d15868176fe871f42a65b9
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-06-01 03:52:14 +0900

    [UPDATE] ALLOWED HOST 추가

--- BackEnd/Django/modelImport/modelImport/settings.py
+++ BackEnd/Django/modelImport/modelImport/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-kbtscp&%ab)oj-ko=%9l2*!r*=&jyd$l%6a#%_+-9$$89^lk#i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.180.88.174']
+ALLOWED_HOSTS = ['54.180.88.174', 'localhost']
 
 
 # Application definition
