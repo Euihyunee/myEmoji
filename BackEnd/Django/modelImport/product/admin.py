670b77c439c5dbf096db2bb137acad250314983d
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-06-01 03:53:06 +0900

    [UPDATE] 사진, 이모티콘 모델 추가

--- BackEnd/Django/modelImport/product/admin.py
+++ BackEnd/Django/modelImport/product/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
 
-from product.models import Product
 
 # Register your models here.
-admin.site.register(Product)
--- BackEnd/Django/modelImport/product/models.py
+++ BackEnd/Django/modelImport/product/models.py
@@ -1,10 +1,21 @@
 from django.db import models
-from diffusers import DiffusionPipeline
+class Style(models.Model):
+    request_id = models.CharField(max_length=50, null=False)
+    tag_name = models.CharField(max_length=50, null=False)
+    img_url = models.CharField(max_length=256, null=False)
+    img = models.BinaryField(null=False)
 
-class Product(models.Model):
-    name = models.CharField(max_length=70)
-    price = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        db_table = 'style'
 
-    def __str__(self):
-        return self.name
+
+class Emoji(models.Model):
+    request_id = models.CharField(max_length=50, null=False)
+    tag_name = models.CharField(max_length=50, null=False)
+    emoji_tag = models.CharField(max_length=50, null=False)
+    emoji_url = models.CharField(max_length=256, null=False)
+    emoji = models.BinaryField(null=False)
+    set_num = models.IntegerField(null=False)
+
+    class Meta:
+        db_table = "emoji"
